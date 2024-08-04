4ad4503daa5af23a7b9cd14168dfe79744a680f0
Anuj Vichare <vichare.anuj@gmail.com>
2024-08-04 18:22:30 +0530

    Tested the code with multithreaded app

--- Tester.cpp
+++ Tester.cpp
@@ -1,16 +1,44 @@
 
 #include "MyLogger.h"
+#include <thread>
+#include<sstream>
+
+void threadFunc(MyLogger& logger, const LogLevel& level)
+{
+	std::stringstream log;
+	log << std::this_thread::get_id();
+	log << ": log statement";
+	for(int i = 0 ; i < 10000; ++i )
+	{
+		switch(level)
+		{
+			case LogLevel::eVerbose :
+				logger.LogV(log.str());
+				break;
+			case LogLevel::eNormal :
+				logger.LogN(log.str());
+				break;
+			case LogLevel::eWarning :
+				logger.LogW(log.str());
+				break;
+			case LogLevel::eError :
+				logger.LogE(log.str());
+				break;
+		};
+	}
+	
+}
 
 int main()
 {
-	MyLogger logger("TestLog", LogLevel::eError);
+	MyLogger logger("TestLog", LogLevel::eVerbose);
+	std::thread t1(threadFunc, std::ref(logger), LogLevel::eVerbose);
+	std::thread t2(threadFunc, std::ref(logger), LogLevel::eNormal);
+	std::thread t3(threadFunc, std::ref(logger), LogLevel::eWarning);
+	std::thread t4(threadFunc, std::ref(logger), LogLevel::eError);
 
-	logger.LogW("warning log ..........");
-	logger.LogN("warning log ..........");
-	logger.LogW("warning log ..........");
-	logger.LogE("warning log ..........");
-	logger.LogE("warning log ..........");
-	logger.LogV("warning log ..........");
-	logger.LogV("warning log ..........");
-	logger.LogV("warning log ..........");
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
 }
